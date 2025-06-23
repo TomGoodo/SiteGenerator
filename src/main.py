d89b4eb80beaa2e52bccffee992b6d3f50e902a6
= <tomrgoodison@gmail.com>
2025-06-23 18:32:53 +1000

    First Commit, added Basic things

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def main():
+
+    print("Hello World")
+    node = TextNode("Text Testing", TextType.TEXT)
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
