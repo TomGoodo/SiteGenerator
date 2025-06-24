fd8fb679b6ef32dfda92fd4278239c85936eb45a
= <tomrgoodison@gmail.com>
2025-06-24 11:10:13 +1000

    Added Test Cases

--- src/textnode.py
+++ src/textnode.py
@@ -19,7 +19,6 @@ class TextNode:
         return (self.text == value.text 
                 and self.text_type == value.text_type 
                 and self.url == value.url)
-    
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
