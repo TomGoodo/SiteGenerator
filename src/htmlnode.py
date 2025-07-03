7755789d63a427c2316ba44ba8f8540eac4adc76
TomGoodo <tomrgoodison@gmail.com>
2025-07-04 09:56:04 +1000

    Added ParentNode and Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -35,3 +35,18 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props )
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag has invalid value")
+        if self.children is None:
+            raise ValueError("Children has invalid value")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}>{children_html}</{self.tag}>"
