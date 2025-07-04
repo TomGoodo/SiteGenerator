8c08e05d51533c5f9c295ae0f021abcb05d80666
TomGoodo <tomrgoodison@gmail.com>
2025-07-04 10:57:32 +1000

    Added TextNode to HtmlNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 
@@ -24,3 +25,23 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type not in TextType:
+        raise Exception("incorrect text type")
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None,text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b",text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i",text_node.text)
+        case TextType.CODE:
+            return LeafNode("code",text_node.text)
+        case TextType.LINK:
+            return LeafNode("a",text_node.text, "href")
+        case TextType.IMAGE:
+            return LeafNode("img","", {"src": text_node.url ,"alt" : text_node.text})
+
+
